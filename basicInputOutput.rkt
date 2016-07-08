b1be297e41d3bd50c0aabfcf0390a57984277e74
squifi <squifi@entwicklerfreu.de>
2016-07-08 21:01:01 +0200

    Added some regexp-replace* examples

--- basicInputOutput.rkt
+++ basicInputOutput.rkt
@@ -3,11 +3,17 @@
 (define initialize
   (copy-file "File.txt" "File.tmp"))
 
+(define (replace-assert t)
+  (display (regexp-replace* #rx"assert" t "Assert."))
+  (display (regexp-replace* #rx"!= null" t "notNull(")))
+
 (define process
   (let ([temp (file->string "File.tmp")])
     [if [< 0 (string-length temp)]
-        (display temp)
+        (replace-assert temp)
         (display "No string in File")]))
+
+
 
 (define final-step
   (delete-file "File.tmp"))
